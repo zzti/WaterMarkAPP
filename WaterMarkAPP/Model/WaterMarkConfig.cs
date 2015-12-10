d9273ebeb4102c5006b2f5e4a276d864f1a92354
dunitian <1054186320@qq.com>
2015-12-10 16:44:39 +0800

    通用水印第二个版本

---
+++ WaterMarkAPP/Model/WaterMarkConfig.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using WaterMarkAPP.Enums;
+
+namespace WaterMarkAPP.Model
+{
+    public class WaterMarkConfig
+    {
+        private string _copyName;
+        /// <summary>
+        /// 文字水印内容
+        /// </summary>
+        public string CopyName
+        {
+            get { return _copyName; }
+            set { _copyName = value; }
+        }
+
+        private string _copyImgUrl;
+        /// <summary>
+        /// 图片水印路径
+        /// </summary>
+        public string CopyImgUrl
+        {
+            get { return _copyImgUrl; }
+            set { _copyImgUrl = value; }
+        }
+
+        private WaterMarkLocationEnum _copyLocation;
+        /// <summary>
+        /// 水印位置
+        /// </summary>
+        public WaterMarkLocationEnum CopyLocation
+        {
+            get { return _copyLocation; }
+            set { _copyLocation = value; }
+        }
+
+        private float _copyOpacity;
+        /// <summary>
+        /// 水印透明度
+        /// </summary>
+        public float CopyOpacity
+        {
+            get { return _copyOpacity; }
+            set { _copyOpacity = value; }
+        }
+    }
+}
